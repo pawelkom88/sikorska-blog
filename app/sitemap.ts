17ce1a9684698c77f463c54f8206fc017ddec26a
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2023-10-08 17:36:34 +0100

    bug fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,13 @@
-import { MetadataRoute } from 'next'
-import { allBlogs } from 'contentlayer/generated'
-import siteMetadata from '@/data/siteMetadata'
+import {MetadataRoute} from "next";
+import siteMetadata from "@/data/siteMetadata";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const siteUrl = siteMetadata.siteUrl
-  const blogRoutes = allBlogs.map((post) => ({
-    url: `${siteUrl}/${post.path}`,
-    lastModified: post.lastmod || post.date,
-  }))
+  const siteUrl = siteMetadata.siteUrl;
 
-  const routes = ['', 'blog', 'projects', 'tags'].map((route) => ({
+  const routes = ["", "blog"].map(route => ({
     url: `${siteUrl}/${route}`,
-    lastModified: new Date().toISOString().split('T')[0],
-  }))
+    lastModified: new Date().toISOString().split("T")[0],
+  }));
 
-  return [...routes, ...blogRoutes]
+  return routes;
 }
